a27f3bdd547ee61f68e17b8a27a76b926ec06c31
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-10-09 11:42:50 +0300

    isolate target data

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,9 +1,19 @@
 class Predator:
-    def __init__(self, a, b, c, d, name, target_data):
+    def __init__(self, a, b, c, d, name):
         self.a = a
         self.b = b
         self.c = c
         self.d = d
         self.name = name
+        self.target_data = None
+
+    def assign_target_data(self, target_data):
         self.target_data = target_data
+
+    def preprocess_target_data(self):
+        if self.target_data is None:
+            raise AttributeError("Target data is not initialized.")
+
+        # Take transpose.
+        self.target_data = self.target_data.T
 
