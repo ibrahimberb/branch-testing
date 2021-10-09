729c406cae3f5456640adf9393f2b4a9981f7eba
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-10-09 11:49:16 +0300

    Update utils.py

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,9 +1,7 @@
-class Predator:
-    def __init__(self, a, b, c, d, name, target_data):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
-        self.name = name
-        self.target_data = target_data
+import pickle
 
+
+def serialize(obj, name):
+    filename = f"{name}.pkl"
+    with open(filename, 'wb') as fid:
+        pickle.dump(obj, fid)
